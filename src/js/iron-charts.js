f4cfe1751881a90c53ee5da93253fb883e24cf43
Denis Michailov <kamrad.reg@gmail.com>
2016-10-13 23:43:39 +1000

    init

---
+++ src/js/iron-charts.js
@@ -0,0 +1,11 @@
+module.exports = (function(){
+
+	var init = function(){
+		console.log('initialization...');
+	};
+
+	return {
+		init: init
+	};
+
+})();
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+var d3 = require("d3");
+var classie = require("./classie");
+var settings = require("./settings");
+
+console.log( "d3 version is " + d3.version );
+
+var ironCharts = require('./iron-charts')
+ironCharts.init();
